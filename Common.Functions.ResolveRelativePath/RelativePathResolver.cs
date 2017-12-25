c12fc958f34c3055c7a3e4252db029a86ec858f2
Christopher Dorst <chris@dorst.email>
2017-12-25 16:53:58 -0600

    Adding RelativePathResolver

---
+++ Common.Functions.ResolveRelativePath/RelativePathResolver.cs
@@ -0,0 +1,10 @@
+﻿using System.IO;
+
+namespace Common.Functions.ResolveRelativePath
+{
+    public static class RelativePathResolver
+    {
+        public static string GetPath(string pathRelativeToCurrent)
+            => Path.Combine(Directory.GetCurrentDirectory(), pathRelativeToCurrent);
+    }
+}
